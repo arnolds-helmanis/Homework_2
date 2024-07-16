b653eec0856c005130e66c02c51f1f50b7648e65
arnolds-helmanis <147103095+arnolds-helmanis@users.noreply.github.com>
2024-07-16 22:52:33 +0300

    Add files via upload

---
+++ PracticalWork1.java
@@ -0,0 +1,35 @@
+
+public class PracticalWork1 {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 6;
+		showRectangleInfo(a, b);
+	}
+
+	public static void showRectangleInfo(int x, int y) {
+		int perimeter = (x + y) * 2;
+		int area = x * y;
+		String rectangle = "rectangle";
+
+		if (x <= 0 || y <= 0) {
+			System.out.println("Invalid parameters.");
+			return;
+		}
+
+		if (x == y) {
+			rectangle = "square";
+			System.out.println("This figure is a " + rectangle + ".");
+		} else {
+			System.out.println("This figure is a " + rectangle + ".");
+		}
+
+		System.out.println("The perimeter of given " + rectangle + " is " + perimeter + " units.");
+		System.out.println("The area of given " + rectangle + " is " + area + " square units.");
+	}
+
+}
+
+//Izveidot metodi showRectangleInfo(), kam tiek padoti abu taisnstūra malu garumi.
+//Izdrukāt metodē masīva perimetru, laukumu un faktu, ka taisnstūris ir kvadrāts (ja
+//tā ir).
